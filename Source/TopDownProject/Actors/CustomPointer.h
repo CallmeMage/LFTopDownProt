1263ad94046dfcde65f088a492057de5165e8113
Laynel Feldshtein <lf@artificialcore.com>
2026-08-14 12:17:13 +0300

    Initial commit

---
+++ Source/TopDownProject/Actors/CustomPointer.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CustomPointer.generated.h"
+
+UCLASS()
+class TOPDOWNPROJECT_API ACustomPointer : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACustomPointer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
