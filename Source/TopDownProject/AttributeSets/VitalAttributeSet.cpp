4a66409d9f457b78c1cc41f1deb902ee3d8f42dd
CallmeMage <laynel.feldshtein@gmail.com>
2026-08-15 21:10:13 +0300

    AttributeSets and Hp Bar
    * Added new AttributeSets for Vitality and Damage;
    * Clamped Hp and Mana attributes to the Max Hp and Max Mana correspondingly;
    * Added debug logic to track 0 hp and 0 mana events;
    * Created Hp Bar to test functionality;

---
+++ Source/TopDownProject/AttributeSets/VitalAttributeSet.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VitalAttributeSet.h"
+
+#include "WorldPartition/ContentBundle/ContentBundleLog.h"
+
+void UVitalAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+	if(Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
+	}
+	if(Attribute == GetManaAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
+	}
+}
+
+void UVitalAttributeSet::PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue)
+{
+	Super::PostAttributeChange(Attribute, OldValue, NewValue);
+	
+	if(Attribute == GetHealthAttribute())
+	{
+		if (NewValue <= 0.f)
+		{
+			UE_LOG(LogTemp, Log, TEXT("Dead"));
+		}
+	}
+	if(Attribute == GetManaAttribute())
+	{
+		if (NewValue <= 0.f)
+		{
+			UE_LOG(LogTemp, Log, TEXT("Out of Mana"));
+		}
+	}
+}
+
---
+++ Source/TopDownProject/AttributeSets/VitalAttributeSet.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TopDownProject/TopDownAttributeSet.h"
+#include "VitalAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWNPROJECT_API UVitalAttributeSet : public UTopDownAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UVitalAttributeSet, Health)
+	
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UVitalAttributeSet, MaxHealth);
+	
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UVitalAttributeSet, Mana)
+	
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UVitalAttributeSet, MaxMana);
+	
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
+	
+};
